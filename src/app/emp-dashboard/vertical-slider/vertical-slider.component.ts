3920ae6a165bf23c1414913d3b27db3aa87fab51
Abh15github <abhitam33@gmail.com>
2024-11-12 18:14:04 +0530

    updated on 12

--- src/app/emp-dashboard/vertical-slider/vertical-slider.component.ts
+++ src/app/emp-dashboard/vertical-slider/vertical-slider.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ComponentRef, HostListener, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { CreateJobPostComponent } from '../create-job-post/create-job-post.component';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-vertical-slider',
@@ -11,11 +12,26 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './vertical-slider.component.html',
   styleUrl: './vertical-slider.component.css'
 })
-export class VerticalSliderComponent {
-  activeStep: number = 1;  // Default to first circle being active
+export class VerticalSliderComponent{
+  activeStep: number = 1; // First step is active by default
 
-  // Method to change active step
-  setActive(step: number) {
-    this.activeStep = step;
+  activateStep(step: number) {
+    this.activeStep = step; // Set the active step to the clicked step
   }
+  isMobileView = false;
+  constructor() {
+    this.checkMobileView(); // Check on initial load
+  }
+
+  // Listen for window resize events
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkMobileView();
+  }
+
+  // Check if the screen width is 768px or smaller
+  checkMobileView() {
+    this.isMobileView = window.innerWidth <= 768;
+  }
+
 }
